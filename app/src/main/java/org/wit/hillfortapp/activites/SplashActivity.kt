47bb070e17da77f6d35732140d5fa2f46193aacf
Foxyf76 <lukefox5@gmail.com>
2019-10-05 13:32:44 +0100

    Initial commit, added splash screen

---
+++ app/src/main/java/org/wit/hillfortapp/activites/SplashActivity.kt
@@ -0,0 +1,31 @@
+package org.wit.hillfortapp.activites
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import org.wit.hillfortapp.MainActivity
+import org.wit.hillfortapp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //hiding title bar of this activity
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        //making this activity full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+
+        //4second splash time
+        Handler().postDelayed({
+            //start main activity
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            //finish this activity
+            finish()
+        },4000)
+
+    }
+}
