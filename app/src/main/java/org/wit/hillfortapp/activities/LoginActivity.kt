9cb1aae92e7bcf3cc896ae7c5e51e18469e42fe2
Foxyf76 <lukefox5@gmail.com>
2019-10-06 12:51:42 +0100

    Added signup layout

--- app/src/main/java/org/wit/hillfortapp/activities/LoginActivity.kt
+++ app/src/main/java/org/wit/hillfortapp/activities/LoginActivity.kt
@@ -1,14 +1,21 @@
 package org.wit.hillfortapp.activities
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import org.wit.hillfortapp.R
+import android.widget.Button
 
-class LoginActivity: AppCompatActivity(){
+
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val button = findViewById<Button>(R.id.signupButton)
+        button.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
